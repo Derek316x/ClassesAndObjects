32635d27cdf921cc131c52d3863e54bb73946caa
Derek316x <ThePro@Zs-MacBook-Pro.local>
2015-06-21 15:30:53 -0400

    bonus done

--- ClassesAndObjectsPairProject/main.m
+++ ClassesAndObjectsPairProject/main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+
 //Bank Account Class
 @interface BankAccount : NSObject
 
@@ -14,9 +15,7 @@
 - (void)withdraw:(int)amountToWithdraw;
 
 - (void)checkBalance;
-
 - (void)setBalance:(int)balanceToSet;
-//- (void)setPatron:(Patron)patronToSet;
 
 - (instancetype)initWithBonus:(int)b;
 
@@ -34,6 +33,10 @@
     return nil;
 }
 
+- (void)setBalance:(int)balanceToSet{
+    balance = balance + balanceToSet;
+}
+
 - (void)deposit:(int)amountToDeposit{
     balance = amountToDeposit + balance;
 }
@@ -43,7 +46,11 @@
 }
 
 - (void)checkBalance{
-    NSLog(@"The balance is: $%d.", balance);
+    NSLog(@"The balance is $%d.", balance);
+}
+
+- (int)getBalance{
+    return balance;
 }
 
 @end
@@ -51,8 +58,8 @@
 //Patron Class
 @interface Patron : NSObject
 
--(void)makeDeposit:(int)amountToDeposit;
--(void)makeWithdrawl:(int)amountToWithdraw;
+-(void)makeDeposit:(int)amountToDeposit IntoAccount:(int)accountAwaitingDeposit;
+-(void)makeWithdrawl:(int)amountToWithdraw FromAccount:(int)accountToWithdrawFrom;
 
 -(void)checkPocket;
 
@@ -62,10 +69,7 @@
 @end
 
 @implementation Patron {
-    //    NSString *name;
-    //    int pocket;
-    int accountNumber;  //private
-    BankAccount *myAccount;
+    BankAccount *myAccounts[3]; //a patron can have a max of 3 accounts
     NSString *name;
     int pocket;
 }
@@ -79,7 +83,37 @@
 }
 
 -(void)setBankAccount:(BankAccount*)account{
-    myAccount = account;
+//    for (int i = 0; i < 3; i++) {
+//        
+//        if (myAccounts[i] == nil) {
+//            
+//            myAccounts[i] = account; //set the last available account
+//            NSLog(@"Account %d was successfully set.",i);
+//            break;
+//        }
+//        else {
+//            NSLog(@"Account %d is already in use.",i);
+//        }
+//    }
+    int accountNumber = 0;
+    
+    if (myAccounts[0] == nil || myAccounts[1] == nil || myAccounts[2] == nil ) {
+        NSLog(@"Enter an account number: 0, 1, or 2:");
+        scanf("%d",&accountNumber);
+        
+        if (myAccounts[accountNumber] == nil) {
+            myAccounts[accountNumber] = account;
+            NSLog(@"Account %d successfully set!",accountNumber);
+        }
+        else {
+            NSLog(@"Error. Account %d is already in use!",accountNumber);
+        }
+        
+    }
+    else {
+        NSLog(@"Error. All accounts in use!");
+    }
+
 }
 
 -(void)setPocket:(int)moneyInPocket{
@@ -90,36 +124,50 @@
     NSLog(@"%@ has $%d in his pocket.",name, pocket);
 }
 
--(void)makeDeposit:(int)amountToDeposit{
-    pocket -= amountToDeposit;
-    [myAccount deposit:amountToDeposit];
-    NSLog(@"%@ deposited $%d.",name,amountToDeposit);
+-(void)makeDeposit:(int)amountToDeposit IntoAccount:(int)accountAwaitingDeposit{
+    if (amountToDeposit <= pocket){
+        pocket -= amountToDeposit;
+        [myAccounts[accountAwaitingDeposit] deposit:amountToDeposit];
+        NSLog(@"%@ deposited $%d.",name,amountToDeposit);
+    }
+    else {
+        NSLog(@"Invalid transaction.");
+    }
+
 }
 
--(void)makeWithdrawl:(int)amountToWithdraw{
-    pocket += amountToWithdraw;
-    [myAccount withdraw:amountToWithdraw];
+-(void)makeWithdrawl:(int)amountToWithdraw FromAccount:(int)accountToWithdrawFrom{
+    
+    int myBalance = [myAccounts[accountToWithdrawFrom] getBalance];
+    
+    if (myBalance >= amountToWithdraw){
+        pocket += amountToWithdraw;
+        [myAccounts[accountToWithdrawFrom] withdraw:amountToWithdraw];
+        NSLog(@"%@ withdrew $%d.",name,amountToWithdraw);
+    }
+    else {
+        NSLog(@"OVERDRAFT. INVALID WITHDRAWL!");
+    }
 }
 
 @end
-
-
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         BankAccount *kenFamilyAccount = [[BankAccount alloc] initWithBonus:100];
+        BankAccount *kenCollegeFund = [[BankAccount alloc] initWithBonus:500];
+        BankAccount *kenSavings = [[BankAccount alloc] initWithBonus:2];
+        BankAccount *kenVacation = [[BankAccount alloc] initWithBonus:0];
         
         Patron *kenneth = [[Patron alloc] initWithName:@"Kenneth"];
         [kenneth setBankAccount:kenFamilyAccount];
+        [kenneth setBankAccount:kenCollegeFund];
+        [kenneth setBankAccount:kenSavings];
+        [kenneth setBankAccount:kenVacation];
         
-        [kenneth checkPocket];
-        [kenFamilyAccount checkBalance];
-        
-        [kenneth setPocket:45];
-        [kenneth makeDeposit:10];
-        [kenFamilyAccount checkBalance];
-        [kenneth checkPocket];
+        Patron *coco = [[Patron alloc] initWithName:@"Coco"];
+        [coco setBankAccount:kenFamilyAccount];
         
     }
     return 0;
