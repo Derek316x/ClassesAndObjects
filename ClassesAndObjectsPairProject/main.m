0cb31bdaa3ae9b29f5ca095f86830cbb3d888a62
Derek316x <ThePro@Zs-MacBook-Pro.local>
2015-06-21 10:18:24 -0400

    first pair commit

---
+++ ClassesAndObjectsPairProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClassesAndObjectsPairProject
+//
+//  Created by Z on 6/21/15.
+//  Copyright (c) 2015 dereknetto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
