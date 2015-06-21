a309fb0a6fb97007674b34bbfdf38baaa3c3a73a
Umar <umahmud@outlook.com>
2015-06-21 11:30:05 -0400

    Bank Account done

--- ClassesAndObjectsPairProject/main.m
+++ ClassesAndObjectsPairProject/main.m
@@ -8,11 +8,52 @@
 
 #import <Foundation/Foundation.h>
 
+@interface BankAccount : NSObject
+
+- (void)deposit:(int)amountToDeposit;
+- (void)withdraw:(int)amountToWithdraw;
+- (void)checkBalance;
+
+@end
+
+@implementation BankAccount{
+    
+    int balance;
+    
+}
+
+- (instancetype)initWithBonus:(int)b {
+    if (self = [super init]) {
+        balance = 100;
+        return self;
+    }
+    return nil;
+}
+
+- (void)deposit:(int)amountToDeposit{
+    balance = amountToDeposit + balance;
+}
+
+- (void)withdraw:(int)amountToWithdraw{
+    balance = balance - amountToWithdraw;
+}
+
+- (void)checkBalance{
+    NSLog(@"Your balance is: %d", balance);
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-        NSLog(@"cool");
+        
+        BankAccount *account1 = [[BankAccount alloc] initWithBonus:100];
+        [account1 checkBalance];
+        
+        
+        
+        
+        
     }
     return 0;
 }
